8c04d26d42dc440f74d8c4cda172918f4c8be9cf
Juan Cruz <juan_pineda_12@hotmail.es>
2024-04-29 19:24:18 -0300

    end

--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -1,13 +1,12 @@
 type CalorieDisplayProps = {
     calories: number,
-    text: string,
-    color: string
+    text: string
 }
 
-export default function CalorieDisplay({calories, text, color} : CalorieDisplayProps) {
+export default function CalorieDisplay({calories, text} : CalorieDisplayProps) {
   return (
         <div className='text-center'>
-            <p className={` font-black text-3xl md:text-5xl text-${color}`}>{calories}</p>
+            <p className={` font-black text-3xl md:text-5xl text-white`}>{calories}</p>
             <p className='text-white md:text-lg'>{text}</p>
         </div>
   )
